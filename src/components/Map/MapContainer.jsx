e1430a396af2ec30b9500e5667a529b1201e8a76
Neuter <mark@air.city>
2024-11-30 22:11:06 +0800

    Add files via upload

--- src/components/Map/MapContainer.jsx
+++ src/components/Map/MapContainer.jsx
@@ -4,7 +4,7 @@ import MapView from "@arcgis/core/views/MapView";
 import Locate from "@arcgis/core/widgets/Locate";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import Graphic from "@arcgis/core/Graphic";
-import geometryEngine from "@arcgis/core/geometry/geometryEngine";
+import { geometryEngine } from "@arcgis/core/geometry/geometryEngine";
 import "./MapContainer.css";
 
 const MapContainer = () => {
