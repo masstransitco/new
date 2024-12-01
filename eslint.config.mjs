0d2b7933554f49998784869f4fdbe56d0f4b4697
Neuter <mark@air.city>
2024-12-01 16:22:56 +0800

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,29 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        AudioWorkletGlobalScope: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    rules: {
+      "no-console": "off", // Disable no-console rule
+    },
+  },
   pluginReact.configs.flat.recommended,
 ];
