7e8bf35c0d686ba529a4a0a250fc526f9397a0ff
Neuter <mark@air.city>
2024-12-04 22:00:13 +0800

    Update EV7.jsx
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Menu/EV7.jsx
+++ src/components/Menu/EV7.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+
 import React from "react";
 import { Canvas } from "@react-three/fiber"; // React Three Fiber renderer
 import { OrbitControls, useGLTF } from "@react-three/drei"; // Helper utilities
