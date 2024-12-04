c7962b00b90293b9f08c634e4a3972a522eb000e
Neuter <mark@air.city>
2024-12-04 22:00:30 +0800

    Update Taxi.jsx
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Menu/Taxi.jsx
+++ src/components/Menu/Taxi.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
