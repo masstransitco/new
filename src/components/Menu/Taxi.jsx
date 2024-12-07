03a4ea800c99fdccff1fb809f70f606ed4fd246b
Neuter <mark@air.city>
2024-12-07 23:41:23 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Menu/Taxi.jsx
+++ src/components/Menu/Taxi.jsx
@@ -39,7 +39,7 @@ ErrorBoundary.propTypes = {
 
 const GLBViewerComponent = ({ modelPath }) => {
   const { scene } = useGLTF(modelPath, true);
-  return <primitive object={scene} scale={[2, 2, 2]} position={[0, 1, 0]} />;
+  return <primitive object={scene} scale={[5, 5, 5]} position={[0, 0.2, 0]} />;
 };
 
 GLBViewerComponent.propTypes = {
