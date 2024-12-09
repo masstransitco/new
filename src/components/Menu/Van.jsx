c5bebcee14fbc520997da7644cb46d31d97127f1
Neuter <mark@air.city>
2024-12-09 18:17:14 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Menu/Van.jsx
+++ src/components/Menu/Van.jsx
@@ -11,7 +11,9 @@ import PropTypes from "prop-types";
 
 const GLBViewerComponent = ({ modelPath }) => {
   const { scene } = useGLTF(modelPath, true);
-  return <primitive object={scene} scale={[1, 1, 1]} position={[0, 1.5, 0]} />;
+  return (
+    <primitive object={scene} scale={[1, 1, 1]} position={[0, 1.5, 0]} />
+  );
 };
 
 GLBViewerComponent.propTypes = {
@@ -27,59 +29,43 @@ const GroundPlaneComponent = ({ isSelected }) => (
 );
 
 const Van = ({ isSelected }) => {
-  const cameraSettings = useMemo(
-    () => ({
-      position: [20, 10, 25],
-      fov: isSelected ? 30 : 45,
-      near: 0.5,
-      far: 100,
-    }),
-    [isSelected]
-  );
+  const cameraSettings = useMemo(() => ({
+    position: [20, 10, 25],
+    fov: isSelected ? 30 : 45,
+    near: 0.5,
+    far: 100,
+  }), [isSelected]);
 
-  const lighting = useMemo(
-    () => (
-      <>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
-      </>
-    ),
-    []
-  );
+  const lighting = useMemo(() => (
+    <>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} castShadow />
+    </>
+  ), []);
 
-  const orbitControlsSettings = useMemo(
-    () => ({
-      autoRotate: isSelected,
-      autoRotateSpeed: isSelected ? 1.5 : 0,
-      enableZoom: false,
-      minDistance: 5,
-      maxDistance: 50,
-      enablePan: false,
-      maxPolarAngle: Math.PI / 2,
-    }),
-    [isSelected]
-  );
+  const orbitControlsSettings = useMemo(() => ({
+    autoRotate: isSelected,
+    autoRotateSpeed: isSelected ? 1.5 : 0,
+    enableZoom: false,
+    minDistance: 5,
+    maxDistance: 50,
+    enablePan: false,
+    maxPolarAngle: Math.PI / 2,
+  }), [isSelected]);
 
   return (
-    <div
-      style={{
-        width: "80px",
-        height: "10vh",
-        overflow: "hidden",
-        borderRadius: "8px",
-        boxShadow: "none", // Remove shadow
-      }}
-    >
-      <Canvas
-        shadows
-        camera={cameraSettings}
-        style={{ width: "100%", height: "100%" }}
-      >
+    <div style={{
+      width: "80px",
+      height: "10vh",
+      overflow: "hidden",
+      borderRadius: "8px",
+      boxShadow: "none", // Remove shadow
+    }}>
+      <Canvas shadows camera={cameraSettings} style={{ width: "100%", height: "100%" }}>
         <Suspense fallback={<Html center>Loading...</Html>}>
           {lighting}
           <OrbitControls {...orbitControlsSettings} />
-          <GroundPlaneComponent isSelected={isSelected} />{" "}
-          {/* Pass selection state */}
+          <GroundPlaneComponent isSelected={isSelected} /> {/* Pass selection state */}
           <Environment preset="studio" />
           <Preload all />
           <GLBViewerComponent modelPath={process.env.PUBLIC_URL + "/Van.glb"} />
