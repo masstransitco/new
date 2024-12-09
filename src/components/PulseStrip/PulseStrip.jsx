553b64f9bb56e284b03f977afacdd161eaaa10de
Neuter <mark@air.city>
2024-12-09 11:44:48 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

---
+++ src/components/PulseStrip/PulseStrip.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PulseStrip.css"; // Import the CSS file for styles
+
+const PulseStrip = () => {
+  return (
+    <div className="loader-container">
+      <div className="pulsating-strip"></div>
+    </div>
+  );
+};
+
+export default PulseStrip;
