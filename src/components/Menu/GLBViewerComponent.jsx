a7fda069beb681db40d6db40541da28513029117
Neuter <mark@air.city>
2024-12-10 17:37:52 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Menu/GLBViewerComponent.jsx
+++ src/components/Menu/GLBViewerComponent.jsx
@@ -1,18 +1,31 @@
 /* eslint-disable react/no-unknown-property */
-
 import React, { memo } from "react";
 import { useGLTF } from "@react-three/drei";
 import PropTypes from "prop-types";
 
-const GLBViewerComponent = memo(({ modelPath, scale }) => {
+const GLBViewerComponent = memo(({ modelPath }) => {
   const { scene } = useGLTF(modelPath, true);
+
+  // Default scale
+  let scale = [1, 1, 1];
+
+  // Check model name and set scale to [3, 3, 3] for EV5, EV7, Taxi, and Van
+  const lowerPath = modelPath.toLowerCase();
+  if (
+    lowerPath.includes("ev5") ||
+    lowerPath.includes("ev7") ||
+    lowerPath.includes("taxi") ||
+    lowerPath.includes("van")
+  ) {
+    scale = [3, 3, 3];
+  }
+
   return <primitive object={scene} scale={scale} position={[0, 1.5, 0]} />;
 });
 
 GLBViewerComponent.displayName = "GLBViewerComponent";
 GLBViewerComponent.propTypes = {
   modelPath: PropTypes.string.isRequired,
-  scale: PropTypes.arrayOf(PropTypes.number),
 };
 
 export default GLBViewerComponent;
