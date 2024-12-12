f5a9e1f9e7e1766f524e13eb75321fbb19c243eb
Neuter <mark@air.city>
2024-12-12 20:50:43 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

---
+++ src/hooks/useFetchGeoJSON.js
@@ -0,0 +1,31 @@
+// src/hooks/useFetchGeoJSON.js
+import { useState, useEffect } from "react";
+
+const useFetchGeoJSON = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data from ${url}`);
+        }
+        const json = await response.json();
+        setData(json.features);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchGeoJSON;
