02c3eb819c11eada49c15e2d53d8b23c70f3e7ce
Neuter <mark@air.city>
2024-12-14 14:18:53 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Header/Game.jsx
+++ src/components/Header/Game.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import { FaTimes } from "react-icons/fa"; // Importing a close icon from react-icons
 
 const MTCGameModal = ({ show, onClose }) => {
+  const modalBodyStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%", // Ensure the modal body takes full height
+  };
+
+  const iframeContainerStyle = {
+    border: "1px solid #555555",
+    borderRadius: "8px",
+    overflow: "hidden", // Ensures content does not overflow outside the rounded corners
+    width: "75%", // Adjust width as needed
+    height: "75vh", // Adjust height as needed
+  };
+
   const iframeStyle = {
-    width: "95%",
-    height: "95vh", // Adjust the height of the iframe as needed
-    border: "none",
+    width: "100%",
+    height: "100%",
+    border: "none", // Remove default iframe border
   };
 
   return (
     <Modal show={show} onHide={onClose} size="lg" centered>
       <Modal.Header closeButton>
-        <Modal.Title>Mass Transit Game</Modal.Title>
+        {/* Custom Close Button */}
+        <Button variant="link" onClick={onClose} aria-label="Close">
+          <FaTimes size={24} color="#555555" /> {/* Using FaTimes icon */}
+        </Button>
       </Modal.Header>
-      <Modal.Body>
-        <iframe
-          src="https://air.city"
-          style={iframeStyle}
-          title="Mass Transit Game"
-          allowFullScreen
-          loading="lazy"
-        ></iframe>
+      <Modal.Body style={modalBodyStyle}>
+        <div style={iframeContainerStyle}>
+          <iframe
+            src="https://air.city"
+            style={iframeStyle}
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onClose}>
