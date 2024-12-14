9fee731f8ada05da44df6126424ad5f04d0a9e62
Neuter <mark@air.city>
2024-12-14 14:20:47 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Scene/SceneContainer.jsx
+++ src/components/Scene/SceneContainer.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect, useRef } from "react";
-import WebScene from "@arcgis/core/WebScene";
-import SceneView from "@arcgis/core/views/SceneView";
+import React, { useEffect, useState } from "react";
+import { APIProvider, Map3DElement } from "@vis.gl/react-google-maps";
 import "./SceneContainer.css";
 
 const SceneContainer = () => {
-  const sceneRef = useRef(null);
+  const [map3DElement, setMap3DElement] = useState(null);
+  const initialPosition = { lat: 22.295, lng: 114.172 }; // Set your initial position
 
   useEffect(() => {
-    const webScene = new WebScene({
-      portalItem: {
-        id: "4304b6c3b2084330b4a2153da9fbbcf0", // Replace with a valid WebScene ID
-      },
-    });
-
-    const sceneView = new SceneView({
-      container: sceneRef.current,
-      map: webScene,
-    });
-
-    return () => {
-      if (sceneView) {
-        sceneView.destroy();
-      }
+    const loadMap = async () => {
+      const { Map3DElement } = await google.maps.importLibrary("maps3d");
+      const newMap3DElement = new Map3DElement({
+        center: {
+          lat: initialPosition.lat,
+          lng: initialPosition.lng,
+          altitude: 16000000,
+        },
+        zoom: 16,
+      });
+      setMap3DElement(newMap3DElement);
+      document.getElementById("scene-container").appendChild(newMap3DElement);
     };
+
+    loadMap();
   }, []);
 
-  return <div ref={sceneRef} style={{ height: "100vh", width: "100%" }} />;
+  return (
+    <APIProvider apiKey="AIzaSyA8rDrxBzMRlgbA7BQ2DoY31gEXzZ4Ours">
+      <div>
+        <div className="pac-card" id="pac-card"></div>
+        <div id="scene-container" style={{ width: "100%", height: "100vh" }} />
+      </div>
+    </APIProvider>
+  );
 };
 
 export default SceneContainer;
