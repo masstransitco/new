245186554b1dc2d007b66a95526be22a213b4ab4
Neuter <mark@air.city>
2024-12-16 01:50:25 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Map/FareInfoWindow.jsx
+++ src/components/Map/FareInfoWindow.jsx
@@ -1,14 +1,17 @@
+// src/components/Map/FareInfoWindow.jsx
+
 import React from "react";
 import PropTypes from "prop-types";
+import { InfoWindow } from "@react-google-maps/api"; // Ensure InfoWindow is imported
 
 const FareInfoWindow = ({ position, fareInfo, onClose }) => {
-  if (!fareInfo) return null;
+  if (!fareInfo || fareInfo.ourFare === undefined) return null;
 
   return (
     <InfoWindow position={position} onCloseClick={onClose}>
       <div>
         <h3>Fare Information</h3>
-        <p>Duration: {fareInfo.duration}</p>
+        <p>Duration: {fareInfo.duration || "N/A"}</p>
         <p>Your Fare: HK${fareInfo.ourFare}</p>
         <p>Estimated Taxi Fare: HK${fareInfo.taxiFareEstimate}</p>
       </div>
@@ -17,7 +20,10 @@ const FareInfoWindow = ({ position, fareInfo, onClose }) => {
 };
 
 FareInfoWindow.propTypes = {
-  position: PropTypes.object.isRequired,
+  position: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+  }).isRequired,
   fareInfo: PropTypes.shape({
     duration: PropTypes.string,
     ourFare: PropTypes.number,
