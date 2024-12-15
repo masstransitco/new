362e6d1e9f05fc534b52c833edcc6bdf3e326a24
Neuter <mark@air.city>
2024-12-16 01:09:52 +0800

    Add files via upload
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Menu/MotionMenu.jsx
+++ src/components/Menu/MotionMenu.jsx
@@ -1,62 +1,16 @@
-// src/components/MotionMenu.jsx
+// src/components/Menu/MotionMenu.jsx
 
 import React from "react";
 import PropTypes from "prop-types";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import { makeStyles } from "@mui/styles";
-
-// Custom styles for the MotionMenu
-const useStyles = makeStyles({
-  menuContainer: {
-    padding: "15px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  fareText: {
-    margin: "5px 0",
-  },
-});
+import "./MotionMenu.css";
 
 const MotionMenu = ({ fareInfo }) => {
-  const classes = useStyles();
-
-  if (!fareInfo) {
-    // Optionally, display a placeholder or nothing if fareInfo is not available
-    return null;
-  }
-
-  const { ourFare, taxiFareEstimate } = fareInfo;
-
   return (
-    <Paper
-      elevation={4}
-      sx={{
-        position: "fixed",
-        bottom: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        bgcolor: "background.paper",
-        p: "15px 20px",
-        borderTopLeftRadius: "12px",
-        borderTopRightRadius: "12px",
-        width: "90%",
-        maxWidth: "400px",
-      }}
-    >
-      <div className={classes.menuContainer}>
-        <Typography variant="h6" className={classes.fareText}>
-          Fare Information
-        </Typography>
-        <Typography variant="body1" className={classes.fareText}>
-          Your Fare: HK${ourFare.toFixed(2)}
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          (Taxi Estimate: HK${taxiFareEstimate.toFixed(2)})
-        </Typography>
-      </div>
-    </Paper>
+    <div className="motion-menu">
+      <h3>Fare Details</h3>
+      <p>Our Fare: HK${fareInfo.ourFare.toFixed(2)}</p>
+      <p>Taxi Estimate: HK${fareInfo.taxiFareEstimate.toFixed(2)}</p>
+    </div>
   );
 };
 
@@ -64,7 +18,7 @@ MotionMenu.propTypes = {
   fareInfo: PropTypes.shape({
     ourFare: PropTypes.number.isRequired,
     taxiFareEstimate: PropTypes.number.isRequired,
-  }),
+  }).isRequired,
 };
 
 export default MotionMenu;
