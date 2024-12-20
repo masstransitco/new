b2c77f136c7198458e730700412450d66b310af2
Neuter <mark@air.city>
2024-12-20 13:09:49 +0800

    Update UserOverlay.jsx
    Signed-off-by: Neuter <mark@air.city>

--- src/components/Map/UserOverlay.jsx
+++ src/components/Map/UserOverlay.jsx
@@ -1,6 +1,45 @@
-// src/components/Map/UserOverlay.jsx
 import React from "react";
 import { OverlayView } from "@react-google-maps/api";
+import styled from "@emotion/styled";
+
+const PulsingDot = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: #1B6CFB;
+  border-radius: 50%;
+  border: 2px solid #FFFFFF;
+  position: relative;
+  transform: translate(-50%, -50%);
+  
+  &::after {
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    right: -2px;
+    bottom: -2px;
+    border-radius: 50%;
+    background-color: #1B6CFB;
+    opacity: 0.4;
+    z-index: -1;
+    animation: pulse 10s ease-in-out infinite;
+  }
+
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+      opacity: 0.4;
+    }
+    50% {
+      transform: scale(1.5);
+      opacity: 0.2;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 0.4;
+    }
+  }
+`;
 
 const UserOverlay = ({ userLocation, mapHeading }) => {
   if (!userLocation) return null;
@@ -10,18 +49,7 @@ const UserOverlay = ({ userLocation, mapHeading }) => {
       position={userLocation}
       mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
     >
-      <div
-        style={{
-          width: 0,
-          height: 0,
-          borderLeft: "10px solid transparent",
-          borderRight: "10px solid transparent",
-          borderTop: "20px solid #2171ec",
-          transform: `rotate(${mapHeading}deg)`,
-          transformOrigin: "center",
-          zIndex: 10,
-        }}
-      />
+      <PulsingDot />
     </OverlayView>
   );
 };
